9584a406b7ad11c60b7b6d26d3a520b422980aa8
Hephaestus <mcmahonmike9@yahoo.com>
2022-04-30 14:12:20 -0500

    added package-lock.json to gitignore

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -34,8 +38,8 @@ function generateMarkdown(data) {
   ## Tests
   ${data.test}
   ## Questions
-  https://github.com/${data.github}
-  ${data.email}
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
 
 `;
 }
